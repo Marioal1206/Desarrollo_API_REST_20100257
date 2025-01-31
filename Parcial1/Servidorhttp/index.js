e77dce88dc0a338e71047b29fa53460f7bc374c6
MarioRodriguez <l20100257@nlaredo.tecnm.mx>
2025-01-31 09:07:29 -0600

    servidor http
    se subió el servidor http

--- Parcial1/Servidorhttp/index.js
+++ Parcial1/Servidorhttp/index.js
@@ -1,12 +1,16 @@
-const http = require('http');
+const http = require('http');//importamos la libreria http
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1';// esta direccion apunta hacia nuetro propio equipo
+const port = 3000;//guardamos el puerto por donde querramos que nuetro servidor corra
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('¡Hola, mundo! Servidor HTTP en Node.js\n');
+const server = http.createServer((req, res) => {// se crea el servidor req represeta la peticion y res representa la respuesta del servidor 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    res.statusCode = 200;// indicamos que la respuesta es satisfactoria 
+    res.setHeader('Content-Type', 'text/plain');//indicamos que la respuesta sera en texto plano
+    res.end('¡Hola, mundo! Servidor HTTP en Node.js\n');// mandamos la respuesta del servidor y cerramos el enlace 
 });
 
 server.listen(port, hostname, () => {
