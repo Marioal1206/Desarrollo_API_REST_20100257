12ceca8ccf13c4a844191776c84a367d1c54558a
MarioRodriguez <l20100257@nlaredo.tecnm.mx>
2025-02-26 07:26:48 -0600

    actualizacion
    espacios

--- Parcial1/CRUD/src/index.js
+++ Parcial1/CRUD/src/index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 
 app.listen(app.get('port'),()=>{
-    console.log("Servidor escuchando en el puerto",app.get('port'));    
-
+    console.log("Servidor escuchando en el puerto",app.get('port'));      
+  
 
 
 })
